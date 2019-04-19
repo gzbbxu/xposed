b2b0fbad1610d9a895f70723f1984a93dfa67d7a
zhoukeke <zhoukeke@perfxlab.com>
2019-04-19 17:35:45 +0800

    hook 微信消息

--- app/src/main/java/com/zkk/xposedinstall/WechatDice.java
+++ app/src/main/java/com/zkk/xposedinstall/WechatDice.java
@@ -1,5 +1,6 @@
 package com.zkk.xposedinstall;
 
+import android.content.ContentValues;
 import android.util.Log;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
@@ -85,6 +86,65 @@ public class WechatDice implements IXposedHookLoadPackage {
                         super.afterHookedMethod(param);
                     }
                 });
+
+
+                XposedHelpers.findAndHookMethod("com.tencent.wcdb.database.SQLiteDatabase", loadPackageParam.classLoader,
+                        "insertWithOnConflict", String.class, String.class, ContentValues.class, int.class, new XC_MethodHook() {
+                            @Override
+                            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                                Log.e("zhouke", ">> beforeHookedMethod insertWithOnConflict");
+                                super.beforeHookedMethod(param);
+                            }
+
+                            @Override
+                            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                                super.afterHookedMethod(param);
+                                Log.e("zhouke", ">> afterHookedMethod insertWithOnConflict >> " + param.args.length);
+                                for (Object obj : param.args) {
+                                    Log.e("zhouke", "obj = " + obj);
+                                }
+                                String tabName = (String) param.args[0];
+                                if (tabName.equals("message")) {
+                                    Log.i("zhouke", "message tabname = " + tabName + ">>");
+                                    ContentValues contentV = (ContentValues) param.args[2];
+                                    String _content = (String) contentV.get("content");
+                                    int _type = (int) contentV.get("type");
+                                    int isSend = (int) contentV.get("isSend");
+
+
+                                    int _paramInt = (int) param.args[3];
+                                    if (_type == 1) {
+                                        Log.i("zhouke", "_contnet : " + _content + ": type = " + _type + ":isSend : " + isSend + "普通文本 :" + (isSend==0?"接受":"发送"));
+                                    } else if (_type == 34) {
+                                        Log.i("zhouke", "_contnet : " + _content + ": type = " + _type + ":isSend : " + isSend + " 语音:" + (isSend==0?"接受":"发送"));
+                                    }
+                                    Log.i("zhouke", "paramInt : " + _paramInt);
+                                } else {
+                                    Log.i("zhouke", "其它 tabname = " + tabName + ">>");
+                                }
+
+
+                           /*     if (param.args != null) {
+                                    String tabName = (String) param.args[0];
+                                    Log.i("zhouke", "tabname = " + tabName + ">>");
+                                    if (null != tabName && tabName.trim().equals("message")) {
+                                        ContentValues contentValues = (ContentValues) param.args[2];
+                                        String contnet = (String) contentValues.get("content");
+                                        String type = (String) contentValues.get("type");
+                                        int paramInt = (int) param.args[3];
+                                        Log.e("zhouke", "contnet : " + contnet + ":type = " + type + "paramInt : = " + paramInt);
+                                    }
+
+                                }*/
+
+                                   /*     04-19 16:49:14.783 18244-18647/? E/zhouke: >> message
+                                        04-19 16:49:14.783 18244-18647/? E/zhouke: >> msgId
+                                        04-19 16:49:14.784 18244-18647/? E/zhouke: >> lvbuffer=[B@43af81c0 bizClientMsgId= createTime=1555663738000 status=3 msgSeq=615870677 type=1 talkerId=33 content=aaa msgSvrId=7745313153899760405 flag=0 bizChatId=-1 msgId=48 isSend=0 talker=wxid_4iiag2f59pbj21
+                                        04-19 16:49:14.784 18244-18647/? E/zhouke: >> 0*/
+
+
+                            }
+                        });
             }
 
         } catch (Exception e) {
